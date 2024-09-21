0d9d4aa14238a8f6c3cc0cc499c102ec29d913dc
MapleWheels <njainanan@hotmail.com>
2024-09-21 07:34:52 -0400

    - More data interface work.

---
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/IPackageDependencyInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Barotrauma.LuaCs.Data;
+
+public interface IPackageDependencyInfo
+{
+    /// <summary>
+    /// Root folder of the content package.
+    /// </summary>
+    public string FolderPath { get; }
+    /// <summary>
+    /// Name of the package.
+    /// </summary>
+    public string PackageName { get; }
+    /// <summary>
+    /// Steam ID of the package. 
+    /// </summary>
+    public int SteamId { get; }
+    /// <summary>
+    /// The dependency package, if found.
+    /// </summary>
+    public ContentPackage DependencyPackage { get; }
+}
---
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/ILocalizationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Barotrauma.LuaCs.Services;
+
+public interface ILocalizationService : IService
+{
+    bool TryLoadFiles(in string[] filePaths);
+    void UnloadAll();
+    string GetLocalizedString(string key, string fallback);
+    string GetLocalizedString(string key, Func<string, string> fbValueFactory);
+}
