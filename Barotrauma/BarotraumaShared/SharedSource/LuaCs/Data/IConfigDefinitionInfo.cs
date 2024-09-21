38f40052ed15f5b7fdad2f1466279690604fdab4
MapleWheels <njainanan@hotmail.com>
2024-09-21 07:34:52 -0400

    - More data interface work.

---
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/IConfigDefinitionInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Barotrauma.LuaCs.Data;
+
+// TODO: Finish
+public interface IConfigDefinitionInfo
+{
+    public string Name { get; }
+    public string PackageName { get; }
+    public string DisplayName { get; }
+    public string DisplayCategory { get; }
+    public string ImageIcon { get; }
+    public ConfigDataType Type { get; }
+    public string DefaultValue { get; }
+}
+
+public enum ConfigDataType
+{
+    
+}
