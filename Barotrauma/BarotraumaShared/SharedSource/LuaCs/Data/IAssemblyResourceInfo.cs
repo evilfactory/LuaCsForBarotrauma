942c1933303d3629193bc8569e4bc17dfa0052d6
MapleWheels <njainanan@hotmail.com>
2024-09-22 04:02:46 -0400

    [As-is state commit]: - Moved platforms and targets flags to dedicated file. - Some localization work. - Preparing to implement data parsers.

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/IAssemblyResourceInfo.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/IAssemblyResourceInfo.cs
@@ -30,18 +30,3 @@ public interface IAssemblyResourceInfo
     /// </summary>
     public Target Targets { get; }
 }
-
-[Flags]
-public enum Platform
-{
-    Linux=0x1, 
-    OSX=0x2, 
-    Windows=0x4
-}
-    
-[Flags]
-public enum Target
-{
-    Client=0x1, 
-    Server=0x2
-}
---
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/ILocalizationResourceInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+namespace Barotrauma.LuaCs.Data;
+
+public interface ILocalizationResourceInfo
+{
+    CultureInfo TargetCulture { get; }
+    ImmutableArray<string> FilePaths { get; }
+}
