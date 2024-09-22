eee316c0e07b9b03387b4712e2b1b787ef295e9d
MapleWheels <njainanan@hotmail.com>
2024-09-22 07:32:44 -0400

    - ModConfig work and resource data contracts.

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/EPlatformsTargets.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/EPlatformsTargets.cs
@@ -16,3 +16,12 @@ public enum Target
     Client=0x1, 
     Server=0x2
 }
+
+[Flags]
+public enum TargetRunMode
+{
+    ClientEnabled = 0x1,
+    ClientAlways = 0x2,
+    ServerEnabled = 0x4,
+    ServerAlways = 0x8
+}
