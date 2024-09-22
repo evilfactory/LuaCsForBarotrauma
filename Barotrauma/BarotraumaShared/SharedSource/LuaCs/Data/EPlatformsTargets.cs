11cb446ddec0b6e73a3eac0e60fc94db11af3ec1
MapleWheels <njainanan@hotmail.com>
2024-09-22 04:02:46 -0400

    [As-is state commit]: - Moved platforms and targets flags to dedicated file. - Some localization work. - Preparing to implement data parsers.

---
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/EPlatformsTargets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Barotrauma.LuaCs.Data;
+
+[Flags]
+public enum Platform
+{
+    Linux=0x1, 
+    OSX=0x2, 
+    Windows=0x4
+}
+    
+[Flags]
+public enum Target
+{
+    Client=0x1, 
+    Server=0x2
+}
--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/IConfigDefinitionInfo.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Data/IConfigDefinitionInfo.cs
@@ -12,9 +12,12 @@ public interface IConfigDefinitionInfo
     public string ImageIcon { get; }
     public ConfigDataType Type { get; }
     public string DefaultValue { get; }
+    
 }
 
 public enum ConfigDataType
 {
-    
+    Boolean, Int32, Int64, Single, Double, String,
+    Color, Vector2, Vector3, List,
+    RangeInt32, RangeSingle, ControlInput
 }
