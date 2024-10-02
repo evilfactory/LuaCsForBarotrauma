c4e602e9856c1cb8442172f61a08b86f9e273ac4
MapleWheels <njainanan@gmail.com>
2024-10-01 23:31:45 -0400

    - Minor work.

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/IContentPackageService.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/IContentPackageService.cs
@@ -6,6 +6,11 @@ namespace Barotrauma.LuaCs.Services;
 
 public interface IContentPackageService : IService
 {
+    /// <summary>
+    /// Tries to parse a package to produce a working ModConfigInfo.
+    /// </summary>
+    /// <param name="package"></param>
+    /// <returns></returns>
     bool TryParsePackage(ContentPackage package);
     ContentPackage Package { get; }
     IModConfigInfo ModConfigInfo { get; }
