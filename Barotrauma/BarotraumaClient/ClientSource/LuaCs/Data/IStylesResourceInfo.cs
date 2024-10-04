6a117d1c833d764e4516d0d9d093b19d35c62470
MapleWheels <njainanan@gmail.com>
2024-10-04 18:52:29 -0400

    Refactor of Package Service to fix 3am programming errors.

--- Barotrauma/BarotraumaClient/ClientSource/LuaCs/Data/IStylesResourceInfo.cs
+++ Barotrauma/BarotraumaClient/ClientSource/LuaCs/Data/IStylesResourceInfo.cs
@@ -1,3 +1,13 @@
-﻿namespace Barotrauma.LuaCs.Data;
+﻿using System.Collections.Immutable;
+
+namespace Barotrauma.LuaCs.Data;
 
 public interface IStylesResourceInfo : IResourceInfo, IResourceCultureInfo, ILazyLoadableResourceInfo, IPackageDependenciesInfo { }
+
+public interface IStylesResourcesInfo
+{
+    /// <summary>
+    /// Collection of loadable styles data.
+    /// </summary>
+    ImmutableArray<IStylesResourceInfo> StylesResourceInfos { get; }
+}
--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/PackageService.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/PackageService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.Globalization;
 using Barotrauma.LuaCs.Data;
 using Barotrauma.LuaCs.Services.Processing;
 
@@ -6,20 +8,35 @@ namespace Barotrauma.LuaCs.Services;
 
 public partial class PackageService : IContentPackageService
 {
+    // mod config / package scanners/parsers
     private readonly Lazy<IXmlModConfigConverterService> _modConfigConverterService;
     private readonly Lazy<ILegacyConfigService> _legacyConfigService;
-    private readonly Lazy<IXmlLocalizationResConverterService> _localizationConverterService;
+    private readonly Lazy<ILuaScriptService> _luaScriptService;
+    private readonly Lazy<ILocalizationService> _localizationService;
+    private readonly Lazy<IPluginService> _pluginService;
+    private readonly IPluginManagementService _pluginManagementService;
+    private readonly IPackageManagementService _packageManagementService;
     private readonly IStorageService _storageService;
     private readonly ILoggerService _loggerService;
     
-    // cctor in server source and client source
+    // .ctor in server source and client source
     
     public ContentPackage Package { get; private set; }
+
+    #region DataContracts
     
     public IModConfigInfo ModConfigInfo { get; private set; }
+    public ImmutableArray<IPackageDependencyInfo> Dependencies => ModConfigInfo?.Dependencies ?? ImmutableArray<IPackageDependencyInfo>.Empty;
+    public ImmutableArray<CultureInfo> SupportedCultures => ModConfigInfo?.SupportedCultures ?? ImmutableArray<CultureInfo>.Empty;
+    public ImmutableArray<IAssemblyResourceInfo> Assemblies => ModConfigInfo?.Assemblies ?? ImmutableArray<IAssemblyResourceInfo>.Empty;
+    public ImmutableArray<ILocalizationResourceInfo> Localizations => ModConfigInfo?.Localizations ?? ImmutableArray<ILocalizationResourceInfo>.Empty;
+    public ImmutableArray<ILuaResourceInfo> LuaScripts => ModConfigInfo?.LuaScripts ?? ImmutableArray<ILuaResourceInfo>.Empty;
 
-    public bool TryParsePackage(ContentPackage package)
+    #endregion
+    
+    public bool TryLoadResourcesInfo(ContentPackage package)
     {
+        // try loading the ModConfig.xml. If it fails, use the Legacy loader to try and construct one from the package structure.
         if (_storageService.TryLoadPackageXml(package, "ModConfig.xml", out var configXml)
             && configXml.Root is not null)
         {
@@ -29,7 +46,7 @@ public partial class PackageService : IContentPackageService
             }
             else
             {
-                _loggerService.LogError($"Failed to parse ModConfig.xml for package {package.Name}");
+                _loggerService.LogError($"Failed to parse ModConfig.xml for package {package.Name}, package mod content not loaded.");
                 return false;
             }
         }
@@ -42,16 +59,36 @@ public partial class PackageService : IContentPackageService
             // vanilla mod or broken
             return false;
         }
-        
-        // load client resources
-        TryParsePackageClient(package);
-        return true;
+
+        throw new NotImplementedException();
     }
 
-    partial void TryParsePackageClient(ContentPackage package);
-    
+    public bool TryLoadAssemblies()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryLoadLocalizations()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryLoadLuaScripts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public partial bool TryLoadStyles();
+
+    public bool TryLoadConfig()
+    {
+        throw new NotImplementedException();
+    }
+
     public void Dispose()
     {
-        // TODO release managed resources here
+        throw new NotImplementedException();
     }
+
+    
 }
