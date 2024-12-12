1a3cff65dbf2bbde45eac898bca77192a78e4adf
MapleWheels <njainanan@gmail.com>
2024-12-12 17:07:08 -0500

    - Work on storage service. Pre-squash commit.

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/PluginManagementService.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/PluginManagementService.cs
@@ -5,48 +5,7 @@ using Microsoft.CodeAnalysis;
 
 namespace Barotrauma.LuaCs.Services;
 
-public class PluginManagementService : IPluginManagementService 
+public class PluginManagementService : IPluginManagementService, IAssemblyManagementService
 {
     
-    
-    public void Dispose()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public FluentResults.Result Reset()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public bool IsAssemblyLoadedGlobal(string friendlyName)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Result<ImmutableArray<T>> GetTypes<T>(ContentPackage package = null, string namespacePrefix = null, bool includeInterfaces = false,
-        bool includeAbstractTypes = false, bool includeDefaultContext = true, bool includeExplicitAssembliesOnly = false)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public ImmutableArray<MetadataReference> GetStandardMetadataReferences()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public ImmutableArray<MetadataReference> GetPluginMetadataReferences()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Result<ImmutableArray<IAssemblyResourceInfo>> GetCachedAssembliesForPackage(ContentPackage package)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Result<ImmutableArray<IAssemblyResourceInfo>> LoadAssemblyResources(ImmutableArray<IAssemblyResourceInfo> resource)
-    {
-        throw new System.NotImplementedException();
-    }
 }
