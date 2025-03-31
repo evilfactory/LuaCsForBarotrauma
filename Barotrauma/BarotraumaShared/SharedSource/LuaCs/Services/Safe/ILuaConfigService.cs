97d36a21155dccd9011501688be3082018f856c2
MapleWheels <njainanan@hotmail.com>
2025-03-31 03:52:45 -0400

    Adjustments to ConfigService interfaces.

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/Safe/ILuaConfigService.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/Safe/ILuaConfigService.cs
@@ -1,8 +1,26 @@
 ﻿using Barotrauma.LuaCs.Configuration;
+using Microsoft.Xna.Framework;
 
 namespace Barotrauma.LuaCs.Services.Safe;
 
 public interface ILuaConfigService : ILuaService
 {
-    
+    // get
+    bool GetConfigBool(string packageName, string configName);
+    int GetConfigInt(string packageName, string configName);
+    float GetConfigFloat(string packageName, string configName);
+    double GetConfigNumber(string packageName, string configName);
+    string GetConfigString(string packageName, string configName);
+    Vector2 GetConfigVector2(string packageName, string configName);
+    Vector3 GetConfigVector3(string packageName, string configName);
+    Color GetConfigColor(string packageName, string configName);
+    // set
+    void SetConfigBool(string packageName, string configName, bool value);
+    void SetConfigInt(string packageName, string configName, int value);
+    void SetConfigFloat(string packageName, string configName, float value);
+    void SetConfigNumber(string packageName, string configName, double value);
+    void SetConfigString(string packageName, string configName, string value);
+    void SetConfigVector2(string packageName, string configName, Vector2 value);
+    void SetConfigVector3(string packageName, string configName, Vector3 value);
+    void SetConfigColor(string packageName, string configName, Color value);
 }
--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/_Interfaces/IConfigService.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/Services/_Interfaces/IConfigService.cs
@@ -13,20 +13,11 @@ namespace Barotrauma.LuaCs.Services;
 
 public partial interface IConfigService : IReusableService, ILuaConfigService
 {
-    /*
-     * Resource Files.
-     */
+    // Config Files/Resources
     Task<FluentResults.Result> LoadConfigsAsync(ImmutableArray<IConfigResourceInfo> configResources);
     Task<FluentResults.Result> LoadConfigsProfilesAsync(ImmutableArray<IConfigProfileResourceInfo> configProfileResources);
-    FluentResults.Result DisposeConfigs(ImmutableArray<IConfigResourceInfo> configResources);
-    FluentResults.Result DisposeConfigsProfiles(ImmutableArray<IConfigProfileResourceInfo> configProfilesResources);
-    FluentResults.Result DisposeConfigs(ContentPackage package);
-    FluentResults.Result DisposeConfigsProfiles(ContentPackage package);
     
-    
-    /*
-     * Immediate mode
-     */
+    // Immediate Mode
     FluentResults.Result<IConfigEntry<T>> AddConfigEntry<T>(ContentPackage package, string name,
         T defaultValue,
         NetSync syncMode = NetSync.None,
@@ -49,28 +40,8 @@ public partial interface IConfigService : IReusableService, ILuaConfigService
         Func<T, bool> valueChangePredicate = null,
         Action<IConfigEntry<T>> onValueChanged = null) where T : IConvertible, IEquatable<T>;
     
-    FluentResults.Result<IConfigEntry<T>> AddConfigEntry<T>(string packageName, string name,
-        T defaultValue,
-        NetSync syncMode = NetSync.None,
-        ClientPermissions permissions = ClientPermissions.None,
-        Func<T, bool> valueChangePredicate = null,
-        Action<IConfigEntry<T>> onValueChanged = null) where T : IConvertible, IEquatable<T>;
-
-    FluentResults.Result<IConfigList> AddConfigList(string packageName, string name,
-        int defaultIndex, IReadOnlyList<string> values,
-        NetSync syncMode = NetSync.None,
-        ClientPermissions permissions = ClientPermissions.None,
-        Func<IConfigList, int, bool> valueChangePredicate = null,
-        Action<IConfigList, int> onValueChanged = null);
-    
-    FluentResults.Result<IConfigRangeEntry<T>> AddConfigRangeEntry<T>(string packageName, string name,
-        T defaultValue, T minValue, T maxValue,
-        Func<IConfigRangeEntry<T>, int> getStepCount,
-        NetSync syncMode = NetSync.None,
-        ClientPermissions permissions = ClientPermissions.None,
-        Func<T, bool> valueChangePredicate = null,
-        Action<IConfigEntry<T>> onValueChanged = null) where T : IConvertible, IEquatable<T>;
-    
+    // Utility
+    FluentResults.Result DisposePackageData(ContentPackage package);
     FluentResults.Result<IReadOnlyDictionary<string, IConfigBase>> GetConfigsForPackage(ContentPackage package);
     FluentResults.Result<IReadOnlyDictionary<string, IConfigBase>> GetConfigsForPackage(string packageName);
     IReadOnlyDictionary<(ContentPackage, string), IConfigBase> GetAllConfigs();
